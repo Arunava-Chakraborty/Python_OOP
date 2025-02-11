893d2f10ef73c5fb97bb62aa38ae8702a4fc752f
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-02-11 16:00:17 +0530

    Inheritance added

--- Encapsulation.py
+++ Encapsulation.py
@@ -12,15 +12,52 @@ print(obj.name)
 #protected Members
 class Protecte_Mebers:
     def __init__(self):
-        self.__age = 40
+        self._age = 40
     def display(self):
-        print(self.__age)
+        print(self._age)
 
 class subclass(Protecte_Mebers):
     def display_(self):
-        print(self.__age)
+        print(self._age)
 
 obj1 = Protecte_Mebers()
 obj1.display()
 
 #Private Members
+class Private:
+    def __init__(self):
+        self.__degree = 'Btech'
+    def display(self):
+        print(self.__degree)
+
+class Private_subclass(Private):
+    def modify(self):
+        self.deg = self.__degree
+    def display_modify(self):
+        print(self.deg)
+
+obj2 = Private()
+obj3 = Private_subclass()
+
+
+
+
+# Python code to illustrate how double
+# underscore at the beginning works
+class Geek:
+    def __init__(self):
+        self.g = self.Pyth()
+
+    def _single_method(self):
+        print("Single")
+    def __double_method(self): # for mangling
+        print("fgfsgs")
+    class Pyth:
+        def __init__(self):
+            self.name = "arunava"
+        def __double_method(self): # for mangling
+            pass
+
+mangling = Geek()
+mangling._single_method()
+print(mangling.g.name)
