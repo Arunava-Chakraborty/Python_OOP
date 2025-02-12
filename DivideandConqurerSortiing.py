55834240f214b64769b645670630c8f9089b3d14
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-02-12 23:52:34 +0530

    Insertion Sort

--- DivideandConqurerSortiing.py
+++ DivideandConqurerSortiing.py
@@ -1,49 +1,33 @@
-# Partition function
-def partition(arr, low, high):
-    # Choose the pivot
-    pivot = arr[high]
+arr = [4,3,7,1,5,2]
+start = 0
+end = len(arr)-1
 
-    # Index of smaller element and indicates
-    # the right position of pivot found so far
-    i = low - 1
 
-    # Traverse arr[low..high] and move all smaller
-    # elements to the left side. Elements from low to
-    # i are smaller after every iteration
-    for j in range(low, high):
+def Partition(arr , start, end):
+    i = start -1
+    pivot = arr[end]
+
+    for j in range(start , end):
         if arr[j] < pivot:
             i += 1
             swap(arr, i, j)
 
-    # Move pivot after smaller elements and
-    # return its position
-    swap(arr, i + 1, high)
+    swap(arr, i + 1, end)
     return i + 1
 
 
-# Swap function
-def swap(arr, i, j):
-    arr[i], arr[j] = arr[j], arr[i]
-
-
-# The QuickSort function implementation
-def quickSort(arr, low, high):
-    if low < high:
+def Quicksort(arr , start , end):
+    if start < end:
         # pi is the partition return index of pivot
-        pi = partition(arr, low, high)
+        pi = Partition(arr, start , end)
 
         # Recursion calls for smaller elements
         # and greater or equals elements
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi + 1, high)
+        Quicksort(arr, start, pi - 1)
+        Quicksort(arr, pi + 1, end)
+    return arr
+def swap(arr , i, j):
+    arr[i], arr[j] = arr[j], arr[i]
 
 
-# Main driver code
-if __name__ == "__main__":
-    arr = [10, 7, 8, 9, 1, 5]
-    n = len(arr)
-
-    quickSort(arr, 0, n - 1)
-
-    for val in arr:
-        print(val, end=" ")
+print(Quicksort(arr,start,end))
