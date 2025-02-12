db0c5dd4574b0d7e56fe8d105ddcc47de21072c8
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-02-12 12:20:45 +0530

    Insertion Sort

--- SortingAlgorithm.py
+++ SortingAlgorithm.py
@@ -1,4 +1,6 @@
 # Selection Sort algorithm in Python
+#selection Sort
+'''''
 def selectionSort(array, size):
     for s in range(size):
         min_idx = s
@@ -21,3 +23,30 @@ selectionSort(data, size)
 
 print('Sorted Array in Ascending Order is :')
 print(data)
+'''
+
+# Creating a function for insertion sort algorithm
+def insertion_sort(list1):
+    # Outer loop to traverse on len(list1)
+    for i in range(1, len(list1)):
+
+        a = list1[i]
+
+        # Move elements of list1[0 to i-1],
+        # which are greater to one position
+        # ahead of their current position
+        j = i - 1
+
+        while j >= 0 and a < list1[j]:
+            list1[j + 1] = list1[j]
+            j -= 1
+
+        list1[j + 1] = a
+
+    return list1
+
+
+# Driver code
+list1 = [7, 2, 1, 6]
+print("The unsorted list is Insertion Sort:", list1)
+print("The sorted new list Insertion Sort is :", insertion_sort(list1))
