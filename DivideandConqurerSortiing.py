6125166ffb4b79064d55999d2974e7e6df45da63
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-02-12 19:00:38 +0530

    Insertion Sort

--- DivideandConqurerSortiing.py
+++ DivideandConqurerSortiing.py
@@ -1,5 +1,49 @@
-arr = [3,5,1,7,32,9,11,8,2]
+# Partition function
+def partition(arr, low, high):
+    # Choose the pivot
+    pivot = arr[high]
 
-start = 0
-end = len(arr)-1
-def quicksort(arr , start , end):
+    # Index of smaller element and indicates
+    # the right position of pivot found so far
+    i = low - 1
+
+    # Traverse arr[low..high] and move all smaller
+    # elements to the left side. Elements from low to
+    # i are smaller after every iteration
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            swap(arr, i, j)
+
+    # Move pivot after smaller elements and
+    # return its position
+    swap(arr, i + 1, high)
+    return i + 1
+
+
+# Swap function
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+# The QuickSort function implementation
+def quickSort(arr, low, high):
+    if low < high:
+        # pi is the partition return index of pivot
+        pi = partition(arr, low, high)
+
+        # Recursion calls for smaller elements
+        # and greater or equals elements
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+
+# Main driver code
+if __name__ == "__main__":
+    arr = [10, 7, 8, 9, 1, 5]
+    n = len(arr)
+
+    quickSort(arr, 0, n - 1)
+
+    for val in arr:
+        print(val, end=" ")
